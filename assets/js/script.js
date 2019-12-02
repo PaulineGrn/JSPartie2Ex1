476d93f03ee4aa6b73c83c0c4a1059db6c5a56b7
PaulineGrn <pauline.gernigon@orange.fr>
2019-12-02 09:55:19 +0100

    JSPartie2Ex1

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+//Déclaration des fonctions
+function img1(){
+  //Déclarations des variables
+var imgVariable=document.getElementById("mouse");
+//image 1 qui apparait au départ de la page
+imgVariable.src="assets/img/tori.jpg";
+}
+function img2(){
+  var imgVar=document.getElementById("mouse");
+  //image 2 qui apparait lors du passage de la souris dessus
+  imgVar.src="assets/img/mountains.png";
+}
